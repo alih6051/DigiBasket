f3026000de1b979b165432e6a24b85621495dfed
alih6051 <alih6051@gmail.com>
2023-01-19 00:59:18 +0530

    day2 tasks completed

---
+++ src/assets/cl/fruits-vegetables/fruitsVegitables.js
@@ -0,0 +1,17 @@
+import appleSeasons from "./appleSeasons.webp";
+import healthyNutrients from "./healthyNutrients.webp";
+import lushLeafy from "./lushLeafy.webp";
+import plumpsVitaminC from "./plumpsVitaminC.webp";
+import usaPears from "./usaPears.webp";
+import winterHarvest from "./winterHarvest.webp";
+import winterWarries from "./winterWarries.webp";
+
+export const fruitsVegitables = [
+  { url: appleSeasons },
+  { url: healthyNutrients },
+  { url: lushLeafy },
+  { url: plumpsVitaminC },
+  { url: usaPears },
+  { url: winterHarvest },
+  { url: winterWarries },
+];
--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,7 +1,14 @@
+import { Box, Text } from "@chakra-ui/react";
+import AdminMenu from "Admin-Side/Admin-Menu/AdminMenu";
+
 import React from "react";
 
 const Admin = () => {
-  return <div>admi</div>;
+  return (
+    <Box bg={"#181e2a"}>
+      <AdminMenu />
+    </Box>
+  );
 };
 
 export default Admin;
