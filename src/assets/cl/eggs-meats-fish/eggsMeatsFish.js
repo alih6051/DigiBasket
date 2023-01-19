fbc23a6b5f3c0c5082237dcd3c783c11e26212f1
shahabhi095 <shahabhi095@gmail.com>
2023-01-19 23:23:06 +0530

    product card

--- src/assets/cl/eggs-meats-fish/eggsMeatsFish.js
+++ src/assets/cl/eggs-meats-fish/eggsMeatsFish.js
@@ -1,10 +1,15 @@
 import certificateForMeat from "./certificateForMeat.webp";
 import freshoMeat from "./freshoMeat.webp";
 import organicGhee from "./organicGhee.webp";
-
-
+import NewNonLogo from "./NewNonLogo.png";
+import Vegsymbol from "./Vegsymbol.png";
 export const eggsMeatsFish = [
   { url: certificateForMeat },
   { url: freshoMeat },
   { url: organicGhee },
 ];
+
+export const CateIcons = {
+  nonveg: NewNonLogo,
+  veg: Vegsymbol,
+};
