0060e158b39375153190a55b9a9dd6c925243021
mdyajul <yajul2530@gmail.com>
2023-01-20 10:58:12 +0530

    kk

--- src/components/home/HomeCom.jsx
+++ src/components/home/HomeCom.jsx
@@ -18,7 +18,6 @@ const HomeCom = () => {
         <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        
         autoplay={{
           delay: 2500,
           disableOnInteraction: false
