1a9fee147bf31517d7bad9cab22c5ecaca8a7734
alih6051 <alih6051@gmail.com>
2023-01-21 18:39:55 +0530

    small change

---
+++ src/components/navbar/NavbarFull.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import NavbarIcon from "./NavbarIcon";
+
+const NavbarFull = () => {
+  return (
+    <Box py={3}>
+      <NavbarIcon />
+    </Box>
+  );
+};
+
+export default NavbarFull;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../theme";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import Navbar from "@/components/navbar/navbar";
+import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/footer";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const showHeader = router.pathname === "/admin" ? false : true;
+
   return (
     <ChakraProvider theme={theme}>
       <Provider store={store}>
-        <Navbar />
+        {showHeader && <Navbar />}
         <Component {...pageProps} />
-        <Footer />
+        {showHeader && <Footer />}
       </Provider>
     </ChakraProvider>
   );
