5d16cf5e181647d5ab4a29f01f64cd0bdb72991d
shahabhi095 <shahabhi095@gmail.com>
2023-03-04 19:30:02 +0530

    error fixed

--- client/src/components/products/Loader/Loader.jsx
+++ client/src/components/products/Loader/Loader.jsx
@@ -6,9 +6,9 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <HStack width={"30%"}  mt={10} m="auto"  display="flex" textAlign="center" bgColor="#e6e6e6" alignItem="center" justifyContent="center">
+    <HStack width={"30%"}  mt={10} m="auto" textAlign="center" bgColor="#e6e6e6"  justifyContent="center">
       <Image
-      boarderRadius="50%"
+      
         src="https://media3.giphy.com/media/xTk9ZvMnbIiIew7IpW/giphy.gif?cid=ecf05e47g8mqqa6q3z0wb5yj4farns9ef29x12b3vawxi1f7&rid=giphy.gif&ct=g"
         alt="loader"
       />
